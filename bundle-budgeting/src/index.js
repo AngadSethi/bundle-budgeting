d08cfd618d25f8728d4bfa3581939fdabee27111
Srivathsan Baskaran <srivathsan.baskaran@sprinklr.com>
2021-06-25 15:38:14 +0530

    Setup BaseWeb UI Library and Styletron Engine for React App

---
+++ bundle-budgeting/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import  {Provider as StyletronProvider} from 'styletron-react';
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {LightTheme, BaseProvider} from 'baseui';
+
+
+const engine = new Styletron();
+
+ReactDOM.render(
+  <StyletronProvider value={engine}>
+  <BaseProvider theme={LightTheme}>
+    <App />
+  </BaseProvider>
+  </StyletronProvider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
