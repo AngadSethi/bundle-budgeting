52627c992c47e8106ff00c5fa09b2e8deb4d87d6
Angad Sethi <angadsethi18@gmail.com>
2021-06-26 23:26:07 +0530

    Copying over Srivathsan's code
    Merging Srivathsan's earlier commit with my code.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import Table from "./Table";
 function App() {
   return (
     <div className="App">
-      <header >
+      <header>
         <p>Bundle Budgeting</p>
-          <div style={{width: '90%' , margin: '0 auto'}}>
-          <Table />
-          </div>
-          
+        <div style={{ width: "90%", margin: "0 auto" }}>
+          <BundleTable />
+        </div>
       </header>
-      <BundleTable />
-     </div>
+    </div>
   );
 }
 
