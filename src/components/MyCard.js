eb1ecf08cc5b8e3ad25834cdd2690fbe16afb622
Angad Sethi <angadsethi18@gmail.com>
2021-07-01 20:43:49 +0530

    Update MyCard.js

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,25 +1,24 @@
 import { Card, StyledBody } from "baseui/card";
 import * as React from "react";
-import { colors } from 'baseui/tokens';
+import { colors } from "baseui/tokens";
 
 function MyCard(props) {
   return (
     <Card
       overrides={{
         Root: {
-          style: {
+          style: ({ $theme }) => ({
             width: "328px",
             height: "200px",
             fontSize: "100px",
-            backgroundColor: colors.blue100,
-            borderWidth: '1px',
-            borderRadius: '20px',
-            borderStyle: 'solid',
-            borderColor: 'hsla(0, 0%, 0%, 0.24)',
+            backgroundColor: $theme.colors.backgroundAccent,
+            borderWidth: "1px",
+            borderRadius: "20px",
+            borderStyle: "solid",
+            borderColor: "hsla(0, 0%, 0%, 0.24)",
 
             ...props.overrides.Root.style,
-
-          },
+          }),
           // ...props.overrides.Root,
         },
       }}
