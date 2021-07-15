9af0b948ff02b1758f1d869ebc47331e7babc047
Srivathsan Baskaran <srivathsan.baskaran@sprinklr.com>
2021-07-16 00:51:39 +0530

    Rewrote Input component of Bundle Overview using React guidelines and changed method for editing Bundle Owner and Budget

--- src/pages/bundle/Overview.js
+++ src/pages/bundle/Overview.js
@@ -4,7 +4,7 @@ import { ListItem, ListItemLabel } from "baseui/list";
 import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
 import { H6 } from "baseui/typography";
 import BundleChart from "./BundleChart";
-
+import { Input } from 'baseui/input'
 import edit from "../../edit.png";
 import save from "../../save.png";
 import { Image } from "react-bootstrap";
@@ -13,81 +13,43 @@ export default function Overview(props) {
   const [OwnerEdit, setOwnerEdit] = useState(false);
   const [BudgetEdit, setBudgetEdit] = useState(false);
   const [ownerName, setOwnerName] = useState("");
+  const [budget, setBudget] = useState("");
   var OwnerName;
   var BudgetValue;
   var OwnerEditIcon;
   var BudgetEditIcon;
 
   useEffect(() => {
-    let elementId = props.bundle.name;
-    var listElement = document.getElementById(elementId);
-    var textElements = listElement.getElementsByTagName("p");
-    var editIcons = listElement.getElementsByTagName("img");
-    OwnerName = textElements[1];
-    BudgetValue = textElements[5];
-    OwnerEditIcon = editIcons[0];
-    BudgetEditIcon = editIcons[1];
-    setOwnerName(OwnerName);
-    addEventListeners(OwnerName);
-    addEventListeners(BudgetValue);
+    setOwnerName(props.bundle.owner);
+    setBudget(props.bundle.budget)
   });
 
-  function addEventListeners(TextField) {
-    TextField.addEventListener("mouseover", () => {
-      TextField.style.cursor = "pointer";
-    });
-    TextField.addEventListener("onblur", () => {
-      TextField.contentEditable = false;
-    });
-    TextField.addEventListener("click", function listenForDoubleClick(e) {
-      let element = e.target;
-      element.contentEditable = true;
-      element.style.width = "300px";
-      setTimeout(function () {
-        if (document.activeElement !== element) {
-          element.contentEditable = false;
-        }
-      }, 300);
-    });
+  // function addEventListeners(TextField) {
+  //   TextField.addEventListener("mouseover", () => {
+  //     TextField.style.cursor = "pointer";
+  //   });
+  //   TextField.addEventListener("onblur", () => {
+  //     TextField.contentEditable = false;
+  //   });
+  //   TextField.addEventListener("click", function listenForDoubleClick(e) {
+  //     let element = e.target;
+  //     element.contentEditable = true;
+  //     element.style.width = "300px";
+  //     setTimeout(function () {
+  //       if (document.activeElement !== element) {
+  //         element.contentEditable = false;
+  //       }
+  //     }, 300);
+  //   });
 
-    TextField.addEventListener("keydown", (event) => {
-      if (event.key === "Enter") {
-        event.preventDefault();
-        event.target.contentEditable = false;
-      }
-    });
-  }
-  function changeBudget(BudgetEdit) {
-    let isEdit = !OwnerEdit;
-    setOwnerEdit(isEdit);
-    if (isEdit === true) {
-      BudgetEditIcon.src = save;
-      console.log(BudgetValue);
-      BudgetValue.style.width = "300px";
-      BudgetValue.contentEditable = true;
-      BudgetValue.style.border = "0.5px solid black";
-    } else {
-      BudgetEditIcon.src = edit;
-      BudgetValue.contentEditable = false;
-      BudgetValue.style.border = "0px";
-    }
-  }
+  //   TextField.addEventListener("keydown", (event) => {
+  //     if (event.key === "Enter") {
+  //       event.preventDefault();
+  //       event.target.contentEditable = false;
+  //     }
+  //   });
+  // }
 
-  function editOwner(OwnerEdit) {
-    let isEdit = !BudgetEdit;
-    setBudgetEdit(isEdit);
-
-    if (isEdit === true) {
-      OwnerEditIcon.src = save;
-      OwnerName.style.width = "300px";
-      OwnerName.contentEditable = true;
-      OwnerName.style.border = "0.5px solid black";
-    } else {
-      OwnerEditIcon.src = edit;
-      OwnerName.contentEditable = false;
-      OwnerName.style.border = "0px";
-    }
-  }
   return (
     <Card
       overrides={{
@@ -122,12 +84,45 @@ export default function Overview(props) {
               }}
             >
               <ListItem>
-                <ListItemLabel description={props.bundle.owner}>
+                <ListItemLabel>
                   Owner
                 </ListItemLabel>
+                <ListItemLabel>{
+                  OwnerEdit ? (
+                    <Input
+                      overrides={{
+                        Root: {
+                          style: {
+                            width: "180px"
+                          }
+                        },
+                        Input: {
+                          style: {
+                            backgroundColor: "white",
+                            textAlign: "center"
+                          }
+                        }
+                      }}
+                      placeholder={ownerName}
+                      onChange={e => { console.log(ownerName); setOwnerName(e.target.value) }}
+                      onBlur={e => { setOwnerName(e.target.value) }}
+                    />
+                  ) : (
+                    <div>{ownerName}</div>
+                  )
+                }
+                </ListItemLabel>
                 <Image
-                  onClick={() => {
-                    editOwner(OwnerEdit);
+                  onClick={(e) => {
+                    let isEdit = !OwnerEdit;
+                    setOwnerEdit(isEdit);
+
+                    if (isEdit === true) {
+                      e.target.src = save;
+                    } else {
+                      e.target.src = edit;
+                    }
+                    console.log(ownerName)
                   }}
                   src={edit}
                   width={"18px"}
@@ -135,18 +130,53 @@ export default function Overview(props) {
                   style={{ float: "left" }}
                 />
               </ListItem>
-              <ListItem>
-                <ListItemLabel description={parseSize(props.bundle.size)}>
+              <ListItem >
+                <ListItemLabel >
                   Size
+                </ListItemLabel>
+                <ListItemLabel>
+                  {parseSize(props.bundle.size)}
                 </ListItemLabel>
               </ListItem>
               <ListItem>
-                <ListItemLabel description={parseSize(props.bundle.budget)}>
+                <ListItemLabel >
                   Budget
                 </ListItemLabel>
+                <ListItemLabel>{
+                  BudgetEdit ? (
+                    <Input
+                      overrides={{
+                        Root: {
+                          style: {
+                            width: "180px"
+                          }
+                        },
+                        Input: {
+                          style: {
+                            backgroundColor: "white",
+                            textAlign: "center"
+                          }
+                        }
+                      }}
+                      placeholder={budget}
+                      onChange={e => { console.log(budget); setBudget(e.target.value) }}
+                      onBlur={e => { setBudget(e.target.value) }}
+                    />
+                  ) : (
+                    <div>{parseSize(budget)}</div>
+                  )
+                }
+                </ListItemLabel>
                 <Image
-                  onClick={() => {
-                    changeBudget(BudgetEdit);
+                  onClick={(e) => {
+                    let isEdit = !BudgetEdit;
+                    setBudgetEdit(isEdit);
+                    if (isEdit === true) {
+                      e.target.src = save;
+                    } else {
+                      e.target.src = edit;
+                    }
+                    console.log(budget)
                   }}
                   src={edit}
                   width={"17px"}
