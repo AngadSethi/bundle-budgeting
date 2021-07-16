ccd0bb138687a704db47e86bf75baffc91c5f9ba
Angad Sethi <angadsethi18@gmail.com>
2021-07-16 16:18:44 +0530

    Reducing the sizes of build outputs
    This commit reduces the sizes of build outputs, and minifies them.

--- src/parseBuildOutput.js
+++ src/parseBuildOutput.js
@@ -1,22 +1,10 @@
 function buildOutput(buildStats) {
   const bundleStats = [];
-  const chunkIdToChunkMapping = new Map();
-
-  for (const chunk of buildStats["chunks"]) {
-    chunkIdToChunkMapping.set(chunk["id"], chunk);
-  }
 
   for (const asset of buildStats["assets"]) {
     const assetDetails = {
       name: asset["name"],
       size: asset["size"],
-      chunks: asset["chunks"].map((chunk) => {
-        const chunkObject = chunkIdToChunkMapping.get(chunk);
-        return {
-          names: chunkObject["names"],
-          size: chunkObject["size"],
-        };
-      }),
       hash: buildStats["hash"],
       timestamp: buildStats["builtAt"],
     };
--- src/shared/endPoints.js
+++ src/shared/endPoints.js
@@ -2,9 +2,9 @@ const BUNDLE_STATS_URL = "./data/builds/";
 const BUNDLE_BUDGETS_URL = "./data/budgets-split.json";
 
 const FILES = [
-  "bundle-stats-1.json",
-  "bundle-stats-2.json",
-  "bundle-stats-3.json",
+  "bundle-stats-1-compressed.json",
+  "bundle-stats-2-compressed.json",
+  "bundle-stats-3-compressed.json",
 ];
 
 export { BUNDLE_STATS_URL, BUNDLE_BUDGETS_URL, FILES };
